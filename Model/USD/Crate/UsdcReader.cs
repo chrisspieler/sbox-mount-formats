ede3a09326cee2d62479a1f50fa55d81f987a230
ducc <spielerc@gmail.com>
2025-08-12 19:32:11 -0600

    Read number of tokens and compressed/uncompressed size

--- Model/USD/Crate/UsdcReader.cs
+++ Model/USD/Crate/UsdcReader.cs
@@ -8,7 +8,7 @@ public class UsdcReader : IFileReader<UsdStage>
 {
 	public static readonly byte[] UsdcMagic = "PXR-USDC"u8.ToArray();
 
-	private record TocSection( string Name, long Start, long End );
+	private record TocSection( string Name, ulong Start, ulong End );
 	
 	public UsdStage ReadFromPath( string filePath )
 	{
@@ -27,23 +27,58 @@ public class UsdcReader : IFileReader<UsdStage>
 
 		// Skip version padding.
 		reader.BaseStream.Position += 5;
-
-		var tocOffset = reader.ReadInt64();
-		reader.BaseStream.Position = tocOffset;
+		
+		var tocOffset = reader.ReadUInt64();
+		reader.BaseStream.Position = (long)tocOffset;
 
 		var sections = new TocSection[reader.ReadUInt64()];
 		for ( int i = 0; i < sections.Length; i++ )
 		{
-			sections[i] = ReadSection();
+			var section = ReadTocSection();
+			sections[i] = section;
+			
 			Log.Info( $"Section #{i} \"{sections[i].Name}\" from 0x{sections[i].Start:X8} to 0x{sections[i].End:X8}" );
+		}
+
+		foreach ( var section in sections )
+		{
+			switch ( section.Name )
+			{
+				case "TOKENS":
+					reader.BaseStream.Position = (long)section.Start;
+					var numTokens = reader.ReadUInt64();
+					var uncompressedSize = reader.ReadUInt64();
+					var compressedSize = reader.ReadUInt64();
+					Log.Info( $"Found TOKENS section. numTokens: {numTokens}, uncompressedSize: {uncompressedSize}, compressedSize: {compressedSize}" );
+					// TODO: Decompress the LZ4-compressed integer array in the token section.
+					break;
+				case "STRINGS":
+				case "FIELDS":
+				case "FIELDSETS":
+				case "PATHS":
+				case "SPECS":
+					break;
+				default:
+					Log.Info( $"Unrecognized section: {section.Name}" );
+					break;
+			}
 		}
 		
 		return new UsdStage( [] );
 
-		TocSection ReadSection() => new TocSection(
-			Name: System.Text.Encoding.UTF8.GetString( reader.ReadBytes( 16 ) ).Trim(),
-			Start: reader.ReadInt64(),
-			End: reader.ReadInt64()
+		TocSection ReadTocSection() => new TocSection(
+			Name: ReadNullTerminatedString( 16 ),
+			Start: reader.ReadUInt64(),
+			End: reader.ReadUInt64()
 		);
+
+		string ReadNullTerminatedString( int maxLength )
+		{
+			Span<byte> str = stackalloc byte[maxLength];
+			reader.BaseStream.ReadExactly( str );
+			var nullIndex = str.IndexOf( (byte)0x0 );
+			var slice = nullIndex < 0 ? str : str[..nullIndex];
+			return System.Text.Encoding.UTF8.GetString( slice );
+		}
 	}
 }
